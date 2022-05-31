e38d2cfc079b6f93705b0730f4c248ec07214014
estudantesilva <104015001+estudantesilva@users.noreply.github.com>
2022-05-30 22:15:01 -0300

    adição do app painel_senhas e criação do modelo de dados

--- painel_senhas/models.py
+++ painel_senhas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone 
 
-# Create your models here.
+class StatusSenha(models.Model): 
+    id_status_senha = models.AutoField(primary_key=True) 
+    nome = models.CharField(max_length=255) 
+
+class Categoria(models.Model): 
+    id_categoria = models.AutoField(primary_key=True) 
+    nome = models.CharField(max_length=255) 
+
+class Tipo(models.Model): 
+    id_tipo = models.AutoField(primary_key=True) 
+    nome = models.CharField(max_length=255) 
+    
+class Senha(models.Model): 
+    id_senha = models.AutoField(primary_key=True) 
+    fk_tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE) 
+    fk_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE) 
+    fk_status_senha = models.ForeignKey(StatusSenha, on_delete=models.CASCADE) 
+    senha = models.IntegerField() 
+    hora_data = models.DateTimeField(default=timezone.now)
