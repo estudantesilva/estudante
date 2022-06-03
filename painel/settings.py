496f46b4dd2e8312ecd5d960401ac109c4bc3743
estudantesilva <104015001+estudantesilva@users.noreply.github.com>
2022-06-02 22:48:28 -0300

    Finalização da avaliação N2

--- painel/settings.py
+++ painel/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'America/Brasilia'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
--- painel_senhas/views.py
+++ painel_senhas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from.models import StatusSenha, Senha
+from django.http import Http404
 
+def index(request):
+    try:
+        status = StatusSenha.objects.get(nome='Na fila')
+    except:
+        raise Http404('Status não encontrado')
+    senhas = Senha.objects.filter(fk_status_senha=status.id_status_senha)
+    return render(request, 'index.html', {'senhas': senhas})
 # Create your views here.
